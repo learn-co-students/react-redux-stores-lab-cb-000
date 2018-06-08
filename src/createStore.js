8f6e37f50e8cbd8afa9af5a96eaad22c540df08a
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-06-08 04:24:36 +0000

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,4 +1,31 @@
+import candyReducer from './reducers/candyReducer'
+import countReducer from './reducers/countReducer'
+
 export default function createStore(reducer){
 // your code here!
+  let state;
+  let listeners= [];
 
-}
+  const subscribe = (listener) => {
+    listeners.push(listener)
+  }
+
+  const getState = () => {
+    return state
+  }
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach(
+      listener => listener()
+    )
+  }
+
+  dispatch({type:"Initial Action"})
+
+    return {
+      getState: getState,
+      dispatch: dispatch,
+      subscribe: subscribe
+      }
+  }
