a8c606bcf2e1b37c0e24c1633a80743f8aca157d
dapawn <dapawn@gmail.com>
2019-06-25 00:23:59 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,4 +1,27 @@
 export default function createStore(reducer){
-// your code here!
+  let state;
+  let listeners = [];
+ 
+  const getState = () => { 
+    return state; 
+  };
+
+  const dispatch = (action) => {
+   state = reducer(state, action)
+   listeners.forEach( listener => listener() ) 
+  }
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+  };
+ 
+  dispatch({type: "INTIALIZE_STATE"});
+
+  return {
+    getState: getState,   
+    dispatch: dispatch,
+    subscribe: subscribe       
+  }
+
 
 }
